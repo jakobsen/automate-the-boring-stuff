e5d45cbc4cd1e291db3a18364c46e5f2a8625151
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-18 12:14:58 +0200

    Improved spacing in the print output of the inventory, based on the number of digits in the biggest value in the dictionary

--- chapter-5/inventory.py
+++ chapter-5/inventory.py
@@ -1,11 +1,17 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+import math
 
 
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
+    most_digits = 0
+    for v in inventory.values():
+        digits = math.log10(v) + 1
+        if digits > most_digits:
+            most_digits = digits
+
     for k, v in inventory.items():
-        print(str(v) + "\t" + str(k))
+        print(str(v).ljust(int(most_digits) + 1) + str(k))
         item_total += v
     print("Total number of items: " + str(item_total))
 
@@ -17,7 +23,7 @@ def addToInventory(inventory, addedItems):
     return inventory
 
 
-inv = {'gold coin': 42, 'rope': 1}
+inv = {'gold coin': 101, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 addToInventory(inv, dragonLoot)
 displayInventory(inv)
