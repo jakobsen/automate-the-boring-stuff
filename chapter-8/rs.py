c86f91cf98a4ece094a2fae81236603cc2b15376
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2019-10-22 18:17:10 +0200

    Endret til å bruke funksjon istedet – mye bedre.

--- chapter-8/rs.py
+++ chapter-8/rs.py
@@ -5,7 +5,7 @@ The user may also choose to specify that the results should be placed on the
 clipboard.
 Usage:
 python re.py "<regex>" - Search all *.txt-files in the current directory and
-                       print the results
+                         print the results
 python re.py "<regex>" "<path>" - Search all *.txt-files in the speicified directory
                                   for the specified regex and print the results.
 python re.py "<regex>" copy - Search all *.txt-files in the current directory and
@@ -20,45 +20,39 @@ import sys
 import re
 import pyperclip
 
-if __name__ == "__main__":
-    regex = re.compile(sys.argv[1])
+
+def search_txt_files(regex, path, copy):
+    regex = re.compile(regex)
     matches = []
-    # Begin with the case that we only get a regex
-    if (len(sys.argv) == 2 or
-            (len(sys.argv) == 3 and sys.argv[2].lower() == "copy")):
-        for filename in os.listdir('.'):
-            if filename.endswith(".txt"):
-                f = open(filename, 'r')
-                text_to_search = f.read()
-                for groups in regex.findall(text_to_search):
-                    matches.append(groups)
-                f.close()
+    for filename in os.listdir(path):
+        if filename.endswith("txt"):
+            f = open(os.path.join(path, filename), 'r')
+            text_to_search = f.read()
+            for groups in regex.findall(text_to_search):
+                matches.append(groups)
+            f.close()
+    if len(matches) == 0:
+        print("No matches were found.")
+        exit(0)
+    else:
         print("The following matches were found:")
         print('\n'.join(matches))
-        if len(sys.argv) == 3 and sys.argv[2].lower() == "copy":
+        if copy:
             pyperclip.copy('\n'.join(matches))
             print("Matches copied to clipboard")
+        exit(0)
 
-    if len(sys.argv) == 3 and sys.argv[2].lower() != "copy":
-        for filename in os.listdir(sys.argv[2]):
-            if filename.endswith(".txt"):
-                f = open(sys.argv[2] + "/" + filename, 'r')
-                text_to_search = f.read()
-                for groups in regex.findall(text_to_search):
-                    matches.append(groups)
-                f.close()
-        print("The following matches were found:")
-        print('\n'.join(matches))
 
-    if len(sys.argv) == 4 and sys.argv[3].lower == "copy":
-        for filename in os.listdir(sys.argv[2]):
-            if filename.endswith(".txt"):
-                f = open(sys.argv[2] + "/" + filename, 'r')
-                text_to_search = f.read()
-                for groups in regex.findall(text_to_search):
-                    matches.append(groups)
-                f.close()
-            print("The following matches were found:")
-            print('\n'.join(matches))
-            pyperclip.copy('\n'.join(matches))
-            print("Matches copied to clipboard")
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) == 4:
+        search_txt_files(sys.argv[1], sys.argv[2], True)
+    elif len(sys.argv) == 3 and sys.argv[2].lower() == "copy":
+        search_txt_files(sys.argv[1], ".", True)
+    elif len(sys.argv) == 3 and sys.argv[2].lower() != "copy":
+        search_txt_files(sys.argv[1], sys.argv[2], False)
+    elif len(sys.argv) == 2:
+        search_txt_files(sys.argv[1], ".", False)
+    else:
+        print("Invalid arguments. Please see below.")
+        print(__doc__)
